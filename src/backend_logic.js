af22250507bbd0868288669afca339a9b7d62232
dan kiss and Ian Goodrich <ian@>
2018-03-13 10:15:05 -0700

    setup checker function fully

--- src/backend_logic.js
+++ src/backend_logic.js
@@ -13,31 +13,42 @@ var sample_array = [
 // var sample_array_best = [1,2,3,4,5,6,7,8,9];
 var sample_array_bad = [4,2,3,4,5,6,7,8,9]
 
-
-var sample_array_good = [8,9,1,2,3,4,5,6,7]
+// var sample_array_good = [8,9,1,2,3,4,5,6,7]
 
 // sample_array_good.forEach
 //
 // sample_array.forEach(checker())
 
-var truth = null;
 function checker(sample_array_good) {
-  console.log('run');
+  var truth = null;
+  var counter = 0;
+  // console.log('run');
   var sample_array_best = [1,2,3,4,5,6,7,8,9];
   for (var i = 0; i < sample_array_best.length; i++) {
+    var sample_array_best_per = [1,2,3,4,5,6,7,8,9];
     sample_array_best.forEach(function(element) {
       if (element == sample_array_good[i]) {
           truth = true;
-          console.log(truth);
+          counter +=1;
+          var removed = element -1;
+          sample_array_best_per.splice(removed,1);
+
+          // console.log(truth);
           return truth;
       } else {
         truth = false;
-        console.log(truth);
+        console.log('up');
         return truth;
       }
     });
+    console.log(counter);
+    if (counter < 9&&sample_array_best_per.length>0){
+      return "Not Valid";
+    }else{
+      return "Valid";
+    }
   }
-  console.log(truth);
 }
 
-checker(sample_array_good);
+// checker(sample_array_good);
+checker(sample_array_bad);
